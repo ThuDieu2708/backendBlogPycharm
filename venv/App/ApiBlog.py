cac72653e6c421925835b6830020030ccaa7b60e
Your Name <you@example.com>
2021-07-06 03:52:54 +0700

    dbapidemo

--- venv/App/ApiBlog.py
+++ venv/App/ApiBlog.py
@@ -1,39 +1,192 @@
 
-from flask import Flask , request
-import DB.Controllers as Controlers
+from flask import Flask , request, jsonify,abort
+import  dbapidemo.venv.DB.Controllers as Controlers
 from flask_cors import CORS
 import json
-
+from werkzeug.utils import secure_filename
+import simplejson as json
+import os
 app = Flask(__name__)
 cors = CORS(app)
 
 controllers = Controlers.SQL()
 controllers.connectDB()
 
-@app.route('/' ,methods=['GET', 'POST'])
-def get_accountAdmin():
-	listAcount = {}
-	lists = controllers.getListAcount()
-	print(lists)
-	for item in lists:
-		listAcount[item[0]] = item[1]
-	res = json.dumps(listAcount)
-	return res
+UPLOAD_PATH = 'C:\\Users\\Thu Dieu\\CongNgheWeb\\reactjs\\Blog\\frontend\\public\\images'
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg','.jfif']
+app.config['UPLOAD_PATH'] = UPLOAD_PATH
+
+
+@app.route('/homepage' , methods=["GET"])
+def get_homepage():
+    lists = controllers.getHomePage()
+    res = json.dumps(lists)
+    return res,200
+
+@app.route('/detail-home/<id>')
+def get_detail_home(id):
+    lists = controllers.getDetailHome(id)
+    res = json.dumps(lists)
+    return res,200
+
+@app.route('/products-cook')
+def get_products_cook():
+    listCook = {}
+    lists = controllers.getProduts()
+    res = json.dumps(lists)
+    return res,200
+
+@app.route('/detail-cook/<id>')
+def get_detail_cook(id):
+    listCook = {}
+    lists = controllers.getDetail(id)
+    res = json.dumps(lists)
+    return res,200
+
+@app.route('/products-cultural')
+def get_products_cultural():
+    lists = controllers.getProdutsVanHoa()
+    res = json.dumps(lists)
+    return res,200
+
+@app.route('/detail-cultural/<id>')
+def get_detail_cultural(id):
+    lists = controllers.getDetailCultural(id)
+    res = json.dumps(lists)
+    return res,200
+
+@app.route('/login' ,methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    print(username)
+    print(password)
+    lists = controllers.getListAcount(username, password)
+    res = json.dumps(lists)
+
+    if lists:
+        return res,200
+    return 'fail', 500
+
+@app.route('/checkinfo' ,methods=['GET', 'POST'])
+def get_account():
+    if request.method == 'POST':
+        print("->" , json.dumps(controllers.getListAcount(request.get_json())))
+        return json.dumps(controllers.getListAcount(request.get_json()))
+    return "fail check info"
+
+@app.route('/checkID' ,methods=['POST'])
+def get_ID():
+    username = request.form['username']
+    id = controllers.getID(username)
+    if id != -1:
+        return {
+            'id': id
+        }, 200
+    return'fail', 500
 
 @app.route('/adduser' , methods=['GET', 'POST'])
 def save_accountUser():
-	try:
-		item = {}
-		if request.method  == 'POST':
-			print(request.get_json())
-			item["tk"] = request.get_json()["email"]
-			item["mk"] = request.get_json()["password"]
-			controllers.saveRegister(item , "accountuser")
-			return request.get_json()
-		else:
-			return "404"
-	except :
-		return "404"
+    try:
+        item = {}
+        if request.method  == 'POST':
+            print(request.get_json())
+            item["tk"] = request.get_json()["email"]
+            item["mk"] = request.get_json()["password"]
+            controllers.saveRegister(item)
+            return "ok"
+        else:
+            return "fail"
+    except :
+        return "fail2"
+
+@app.route('/addAmThuc' , methods=['GET', 'POST'])
+def save_AmThuc():
+    id = request.form['id']
+    tenmon = request.form['tenmon']
+    nguyenlieu = request.form['nguyenlieu']
+    soche = request.form['soche']
+    author = request.form['author']
+
+    try:
+        state = request.form['state']
+    except:
+        state = 0
+
+    try:
+        uploaded_file = request.files['file']
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    except:
+        filename = request.form['file']
+    controllers.saveAmThuc2(id, nguyenlieu, tenmon, soche, filename, author, state)
+    return "ok"
+
+
+@app.route('/addVanHoa' , methods=['POST'])
+def save_VanHoa():
+    id = request.form['id']
+    tenbai = request.form['tenbai']
+    noidung = request.form['noidung']
+    author = request.form['author']
+    try:
+        state = request.form['state']
+    except:
+        state = 0
+    try:
+        uploaded_file = request.files['file']
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    except:
+        filename = request.form['file']
+    controllers.saveVanHoa(id, tenbai, noidung, filename, author, state)
+    return "ok"
+
+@app.route('/listByName' , methods=['POST'])
+def getListByName():
+    try:
+        if request.method  == 'POST':
+
+            return json.dumps(controllers.getListByAuthor(request.get_json()))
+        else:
+            return "fail getListByName"
+    except :
+        return "fail2 getListByName"
+
+
+@app.route('/deleteId' , methods=['POST'])
+def deleteId():
+    try:
+        if request.method  == 'POST':
+            controllers.deleteId(request.get_json())
+            return "ok"
+        else:
+            return "fail deleteId"
+    except :
+        return "fail2 deleteId"
+
+# -------------bài đăng user---------------
+@app.route('/userCook' , methods=['GET'])
+def userCoook():
+    lists = controllers.getUser_Cook()
+    res = json.dumps(lists)
+    return res, 200
+
+@app.route('/userCultural' , methods=['GET'])
+def userCultural():
+    lists = controllers.getUser_Cultural()
+    res = json.dumps(lists)
+    return res, 200
+
 
 if __name__ == '__main__':
-	app.run();
+    app.run(debug=True)
+
--- venv/DB/Controllers.py
+++ venv/DB/Controllers.py
@@ -1,52 +1,402 @@
 import sys
-
-import DB.config as Config
+import json
+from flask import jsonify,make_response
+import dbapidemo.venv.DB.config as Config
 import pymysql
 
 
+def covertStr(val):
+    return "\"" + val + "\""
+
+
 class SQL:
-	connection = None
+    connection = None
 
-	def connectDB(self):
-		try:
-			self.connection = pymysql.connect(host=Config.data["host"], user=Config.data["user"],
-											  passwd=Config.data["passwd"], database=Config.data["database"])
-			print("connect thanh cong")
-		except:
-			print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra.")
-			print("turn on xmapp")
-			print("connect that bai");
+    def connectDB(self):
+        try:
+            self.connection = pymysql.connect(host=Config.data["host"], user=Config.data["user"],
+                                              passwd=Config.data["passwd"], database=Config.data["database"])
+            print("connect thanh cong")
+        except:
+            print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra.")
+            print("turn on xmapp")
+            print("connect that bai");
 
-	def setCursor(self):
-		if self.connection != None:
-			return self.connection.cursor()
-		else:
-			print("self.connection chưa khởi tạo , call method connectDB()")
-			return None
+    def setCursor(self):
+        try:
+            if self.connection != None:
+                return self.connection.cursor()
+            else:
+                print("self.connection chưa khởi tạo , call method connectDB()")
+                return None
+        except:
+            print("error here setCursor")
+            return None
 
-	# admin or user
-	def getListAcount(self, tableName='admin'):
-		if self.connection != None:
-			cursor = self.setCursor();
-			if cursor != None:
-				sql = "SELECT * FROM " + " acount_" + tableName + ";"
-				cursor.execute(sql)
-				return cursor.fetchall()
-			else:
-				print("cursor is None")
-		else:
-			print("self.connection chưa khởi tạo , call method connectDB()")
+    # admin or user
+    def getListAcount(self , val , tableName="acount"):
+        if self.connection != None:
+            cursor = self.setCursor()
+            if cursor != None:
+                try:
+                    if val["admin"] == 1:
+                        tableName  = "acount_admin"
+                    # sql = "SELECT * FROM " + tableName + " WHERE username = " + "\"" + str(val["email"]) + "\"" + " AND password = " + "\"" + str(val["password"]) + "\"" + ";"
+                    sql = "SELECT * FROM " + tableName + " WHERE username = " + covertStr(val["email"]) + " AND password = " + covertStr(val["password"]) + ";"
+                    print(sql)
+                    cursor.execute(sql)
 
-	# save account user
-	def saveRegister(self, data, tableName='user'):
-		if self.connection != None:
-			cursor = self.setCursor();
-			if cursor != None:
-				sql = "INSERT INTO " + tableName + " (tk , mk) " + "VALUES(%s , %s)"
+                    return cursor.fetchall()
 
-				cursor.execute(sql , (data["tk"] , data["mk"]))
-				self.connection.commit();
-			else:
-				print("cursor is None")
-		else:
-			print("self.connection chưa khởi tạo , call method connectDB()")
+                except:
+                    return
+            else:
+                return
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    # save account user
+    def saveRegister(self, data, tableName='acount'):
+        if self.connection != None:
+            cursor = self.setCursor()
+            if cursor != None:
+                sql = "INSERT INTO " + tableName + " (username , password) " + "VALUES(%s , %s)"
+                cursor.execute(sql , (data["tk"] , data["mk"]))
+                self.connection.commit()
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def getHomePage(self, tableName='homepage'):
+        if self.connection != None:
+            cursor = self.setCursor()
+            if cursor != None:
+                try:
+
+                    sql = "SELECT * FROM " + tableName + ";"
+
+                    print(sql)
+                    cursor.execute(sql)
+                    lists = cursor.fetchall()
+                    listProducts = []
+
+                    for list in lists:
+                        text = {
+                            'id': list[0],
+                            'tilte': list[1],
+                            'name': list[2],
+                            'noidung': list[3],
+                            'image': list[4]
+                        }
+
+                        listProducts.append(text)
+                    return listProducts
+                except:
+                    print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra. getHomePage")
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+
+    def getDetailHome(self, id):
+        if self.connection != None:
+            cursor = self.setCursor();
+            if cursor != None:
+                sql = "SELECT * FROM homepage WHERE id=" + id + ";"
+                cursor.execute(sql)
+                lists = cursor.fetchall()
+                listProducts = []
+                for list in lists:
+                    text = {
+                        'id': list[0],
+                        'tilte':list[1],
+                        'name': list[2],
+                        'noidung': list[3],
+                        'image': list[4]
+                    }
+                    listProducts.append(text)
+                return listProducts
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def getProduts(self, tableName='cook'):
+        if self.connection != None:
+            cursor = self.setCursor();
+            if cursor != None:
+                sql = "SELECT * FROM " + tableName + ";"
+                cursor.execute(sql)
+                lists = cursor.fetchall()
+                listProducts = []
+                for list in lists:
+                    text = {
+                        'id': list[0],
+                        'name':list[1],
+                        'nguyenlieu': list[2],
+                        'soche': list[3],
+                        'image': list[5]
+                    }
+                    listProducts.append(text)
+                return listProducts
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def getDetail(self, id):
+        if self.connection != None:
+            cursor = self.setCursor();
+            if cursor != None:
+                sql = "SELECT * FROM cook WHERE id=" + id + ";"
+                cursor.execute(sql)
+                lists = cursor.fetchall()
+                listProducts = []
+                for list in lists:
+                    text = {
+                        'id': list[0],
+                        'video': list[6],
+                        'name':list[1],
+                        'nguyenlieu': list[2],
+                        'soche': list[3],
+                        'thuchien': list[4]
+                    }
+                    listProducts.append(text)
+                return listProducts
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def getProdutsVanHoa(self, tableName='cultural'):
+        if self.connection != None:
+            cursor = self.setCursor();
+            if cursor != None:
+                sql = "SELECT * FROM " + tableName + ";"
+                cursor.execute(sql)
+                lists = cursor.fetchall()
+                listProducts = []
+                for list in lists:
+                    text = {
+                        'id': list[0],
+                        'ten':list[1],
+                        'tacgia': list[2],
+                        'noidung': list[3],
+                        'image': list[4]
+                    }
+                    listProducts.append(text)
+                return listProducts
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def getDetailCultural(self, id):
+        if self.connection != None:
+            cursor = self.setCursor();
+            if cursor != None:
+                sql = "SELECT * FROM cultural WHERE id=" + id + ";"
+                cursor.execute(sql)
+                lists = cursor.fetchall()
+                listProducts = []
+                for list in lists:
+                    text = {
+                        'id': list[0],
+                        'ten':list[1],
+                        'tacgia': list[2],
+                        'noidung': list[3],
+                        'image': list[4]
+                    }
+                    listProducts.append(text)
+                return listProducts
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def saveAmThuc2(self, id, nguyenlieu, tenmon, soche, img, author, state):
+        try:
+            if self.connection != None:
+                cursor = self.setCursor()
+                if cursor != None:
+                    try:
+                        sql = "INSERT INTO writecook (id ,tenmon, nguyenlieu ,soche , img  , state , tag, IDuser) " + "VALUES (%s , %s, %s ,%s , %s, %s , %s , %s);"
+                        cursor.execute(sql, (id, tenmon, nguyenlieu, soche, img,state, 'amthuc', str(author)))
+                        self.connection.commit()
+
+                    except:
+                        print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra. at saveAmThuc")
+                else:
+                    print("cursor is None")
+            else:
+                print("self.connection chưa khởi tạo , call method connectDB()")
+        except:
+            print("error here saveAmThuc")
+
+    def saveVanHoa(self, id, tenbai, noidung, img, author, state):
+        try:
+            if self.connection != None:
+                cursor = self.setCursor()
+                if cursor != None:
+                    try:
+                        sql = "INSERT INTO writevanhoa (id ,tenbai, noidung, img , state , tag, IDuser) " + "VALUES (%s , %s, %s , %s, %s , %s , %s);"
+
+                        cursor.execute(sql, (id, tenbai, noidung, img,state, 'vanhoa',str(author)))
+                        self.connection.commit()
+
+                    except:
+                        print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra. at saveVanHoa")
+                else:
+                    print("cursor is None")
+            else:
+                print("self.connection chưa khởi tạo , call method connectDB()")
+        except:
+            print("error here saveVanHoa")
+
+
+    def getListByAuthor(self, val):
+        global tableName
+
+        try:
+            if self.connection != None:
+                cursor = self.setCursor()
+                if cursor != None:
+                    try:
+                        if val["tag"] == "vanhoa":
+                            tableName = "writevanhoa"
+                        elif val["tag"] == "amthuc":
+                            tableName = "writecook"
+                        print(val)
+                        if val["author"] == None:
+                            sql ="select * from " + tableName +  " ORDER BY state asc , IDuser asc;"
+                        else:
+                            sql = "select * from " + tableName + " where IDuser = " + covertStr(val["author"]) + " ORDER BY state asc , IDuser asc;"
+                        print(sql)
+                        cursor.execute(sql)
+                        return cursor.fetchall()
+                    except:
+                        print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra at getListByAuthor")
+                else:
+                    print("cursor is None")
+            else:
+                print("self.connection chưa khởi tạo , call method connectDB()")
+        except:
+            print("error here getListByAuthor")
+
+    def deleteId(self, val):
+        global tableName
+        try:
+            if self.connection != None:
+                cursor = self.setCursor()
+                if cursor != None:
+                    try:
+                        if val["tag"] == "vanhoa":
+                            tableName = "writevanhoa"
+                        elif val["tag"] == "amthuc":
+                            tableName = "writecook"
+                        sql = "delete from " + tableName + " where id = " + covertStr(val["id"]) + ";"
+                        cursor.execute(sql)
+                        self.connection.commit()
+                    except:
+                        print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra at getListByAuthor")
+                else:
+                    print("cursor is None")
+            else:
+                print("self.connection chưa khởi tạo , call method connectDB()")
+        except:
+            print("error here getListByAuthor")
+
+
+ # ----------------------------------------bài đăng user----------------------------------
+    def getUser_Cook(self):
+        if self.connection != None:
+            cursor = self.setCursor()
+            if cursor != None:
+                try:
+                    sql = "SELECT * FROM writecook WHERE state = 1 ;"
+
+                    cursor.execute(sql)
+
+                    lists = cursor.fetchall()
+                    listProducts = []
+
+                    for list in lists:
+
+                        text = {
+                            'id': list[0],
+                            'tenmon': list[1],
+                            'nguyenlieu': list[2],
+                            'soche': list[3],
+                            'image': list[4],
+                            'author': list[5]
+                        }
+
+                        listProducts.append(text)
+                    return listProducts
+                except Exception:
+                    print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra. getUser_Cook")
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+    def getUser_Cultural(self):
+        if self.connection != None:
+            cursor = self.setCursor()
+            if cursor != None:
+                try:
+                    sql = "SELECT * FROM writevanhoa WHERE state = 1 ;"
+                    print(sql)
+                    cursor.execute(sql)
+                    lists = cursor.fetchall()
+                    listProducts = []
+                    for list in lists:
+
+                        name = "SELECT username FROM acount WHERE ID = " + str(list[6]) + ";"
+                        cursor.execute(name)
+                        listss = cursor.fetchall()
+                        name = listss[0][0]
+
+                        text = {
+                            'id': list[0],
+                            'tenbai': list[1],
+                            'noidung': list[2],
+                            'image': list[3],
+                            'author': name,
+                        }
+                        listProducts.append(text)
+                    return listProducts
+                except:
+                    print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra. getUser_Cook")
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
+
+
+
+    def getID(self, username):
+        if self.connection != None:
+            table = "acount"
+            if username == 'admin':
+                table = "acount_admin"
+
+            cursor = self.setCursor()
+            if cursor != None:
+                try:
+                    sql = "SELECT * FROM "+ table+ " WHERE username = '" + str(username)+"';"
+
+                    cursor.execute(sql)
+                    lists = cursor.fetchall()
+                    if lists:
+                       return lists[0][0]
+
+                    return -1
+                except:
+                    print("Có ngoại lệ ", sys.exc_info()[0], " xảy ra. getUser_Cook")
+            else:
+                print("cursor is None")
+        else:
+            print("self.connection chưa khởi tạo , call method connectDB()")
